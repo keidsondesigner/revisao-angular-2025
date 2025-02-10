65af49440193bb2abe8232e359283e9e75c12d79
Keidson Roby <keidsondesigner@gmail.com>
2025-02-09 20:44:47 -0400

    feat(filter): Armazenando os Valores do Filtro

--- 02-projeto-filtro-lista-de-usuarios/src/app/components/filter/filter.component.ts
+++ 02-projeto-filtro-lista-de-usuarios/src/app/components/filter/filter.component.ts
@@ -1,15 +1,43 @@
 import { Component } from '@angular/core';
 
+interface IFilterOptions {
+  name: string;
+  startDate: Date | undefined;
+  endDate: Date | undefined;
+  status: boolean | undefined;
+}
+
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent {
-  statuses = [
-    { value: 'active', label: 'Ativo' },
-    { value: 'inactive', label: 'Inativo' },
-    { value: 'pending', label: 'Pendente' },
+  statusList = [
+    { value: true, label: 'Ativo' }, 
+    { value: false, label: 'Inativo' }
   ];
+
+  // Como não estou usando <form>formulário,
+  // não preciso usar "name" em cada campo do html 
+  filterOptions: IFilterOptions = {
+    name: '',
+    startDate: undefined,
+    endDate: undefined,
+    status: undefined,
+  };
+
+
+  constructor() {}
+
+
+  handleStartDateChange(date: Date) {
+    // Pegando o valor do [(ngModel)] com (ngModelChange)
+    console.log(date);
+  }
+
+  applyFilter() {
+    console.log(this.filterOptions);
+  }
 
 }
